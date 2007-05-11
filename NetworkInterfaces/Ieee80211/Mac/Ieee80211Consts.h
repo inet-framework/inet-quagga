51926b9cb02734b71bf90a52d810eb719b6288c5
levy <levy@539eec35-cc16-0410-b457-356aab9780e1>
2007-05-11 09:02:26 +0000

    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/branches/tmp-omnetpp40port@1549 539eec35-cc16-0410-b457-356aab9780e1

--- NetworkInterfaces/Ieee80211/Mac/Ieee80211Consts.h
+++ NetworkInterfaces/Ieee80211/Mac/Ieee80211Consts.h
@@ -33,10 +33,11 @@ const unsigned int LENGTH_ACK = 112;
 
 // time slot ST, short interframe space SIFS, distributed interframe
 // space DIFS, and extended interframe space EIFS
-const simtime_t ST = 20E-6;
-const simtime_t SIFS = 10E-6;
-const simtime_t DIFS = 2*ST + SIFS;
-const simtime_t MAX_PROPAGATION_DELAY = 2E-6;  // 300 meters at the speed of light
+
+const_simtime_t ST = 20E-6;
+const_simtime_t SIFS = 10E-6;
+const_simtime_t DIFS = 2 * ST + SIFS;
+const_simtime_t MAX_PROPAGATION_DELAY = 2E-6;  // 300 meters at the speed of light
 
 const int RETRY_LIMIT = 7;
 
@@ -46,10 +47,10 @@ const int CW_MIN = 31;
 /** Maximum size of contention window */
 const int CW_MAX = 255;
 
-const int PHY_HEADER_LENGTH=192;
-const int HEADER_WITHOUT_PREAMBLE=48;
-const double BITRATE_HEADER=1E+6;
-const double BANDWIDTH=2E+6;
+const int PHY_HEADER_LENGTH = 192;
+const int HEADER_WITHOUT_PREAMBLE = 48;
+const double BITRATE_HEADER = 1E+6;
+const double BANDWIDTH = 2E+6;
 
 #endif
 
